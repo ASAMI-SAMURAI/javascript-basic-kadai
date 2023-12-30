8898c8e345c2cddd428a531e0afbd0634339fee5
ASAMI-SAMURAI <148980348+ASAMI-SAMURAI@users.noreply.github.com>
2023-12-31 00:28:44 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,15 @@
+for (let num = 1; num <= 100; num++) {
+  if (num % 3 === 0) {
+      console.log("3の倍数です");
+    } 
+    else if (num % 5 === 0) {
+      console.log("5の倍数です")
+    }
+    if (num % 3 === 0 && num % 5 === 0) {
+      console.log("3と5の倍数です")
+    }
+    
+    else {
+      console.log(num);
+    }
+  }
